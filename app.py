5b934c89c32c22aaa17aa976671be9f7e84a565a
tatyana-perlova <tatyana.perlova@gmail.com>
2018-01-27 21:06:49 -0500

    add flask instance

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import colorlover as cl
+import flask
 
+
+server = flask.Flask(__name__)
 app = dash.Dash(__name__)
-server = app.server
 app.config.supress_callback_exceptions = True
 
 
